0b469c53feeb2607d21515d9a33b616cb0db5c01
TigranAteyan <tigranateyan2009@gmail.com>
2025-07-15 14:55:12 +0400

    Added library glfw

---
+++ build/CMakeFiles/BattleCity.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Tigran/Desktop/ITV/Game/BattleCity/src/main.cpp" "CMakeFiles/BattleCity.dir/src/main.cpp.o" "gcc" "CMakeFiles/BattleCity.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/c/Users/Tigran/Desktop/ITV/Game/BattleCity/build/external/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
